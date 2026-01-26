464bc595c2781a660ff67f910363ac75a57e11cf
Andres Romero <andresfromero328@gmail.com>
2026-01-26 14:35:11 -0800

    worked on project request & change layout of home and engineering page

--- lib/actions/createRepairReqAction.ts
+++ lib/actions/createRepairReqAction.ts
@@ -1,33 +1,33 @@
 "use server";
 
-import { RepairRequest } from "../types";
+// import { RepairRequest } from "../types";
 
-export const createRepairReq = async (
-  prevState: RepairRequest,
-  formData: FormData
-) => {
-  const formInput: RepairRequest["formInput"] = {
-    uploadImage: null,
-    uploadVideo: null,
-    brand: (formData.get("brand") as string) ?? "",
-    model: (formData.get("model") as string) ?? "",
-    modelNumber: (formData.get("modelNumber") as string) ?? "",
-    issueTitle: (formData.get("issueTitle") as string) ?? "",
-    issueDate: formData.get("issueDate")
-      ? new Date(formData.get("issueDate") as string)
-      : null,
-    previousWork: (formData.get("previousWork") as string) ?? "",
-    issueOcccurance: (formData.get("issueOcccurance") as string) ?? "",
-    warranty: (formData.get("warranty") as string) ?? "",
-    prefferedDelivery: (formData.get("prefferedDelivery") as string) ?? "",
-    preferredContactMethod:
-      (formData.get("preferredContactMethod") as string) ?? "",
-    issueDescription: (formData.get("issueDescription") as string) ?? "",
-  };
-  return {
-    success: true,
-    message: "",
-    error: "",
-    formInput,
-  };
-};
+// export const createRepairReq = async (
+//   prevState: RepairRequest,
+//   formData: FormData
+// ) => {
+//   const formInput: RepairRequest["formInput"] = {
+//     uploadImage: null,
+//     uploadVideo: null,
+//     brand: (formData.get("brand") as string) ?? "",
+//     model: (formData.get("model") as string) ?? "",
+//     modelNumber: (formData.get("modelNumber") as string) ?? "",
+//     issueTitle: (formData.get("issueTitle") as string) ?? "",
+//     issueDate: formData.get("issueDate")
+//       ? new Date(formData.get("issueDate") as string)
+//       : null,
+//     previousWork: (formData.get("previousWork") as string) ?? "",
+//     issueOcccurance: (formData.get("issueOcccurance") as string) ?? "",
+//     warranty: (formData.get("warranty") as string) ?? "",
+//     prefferedDelivery: (formData.get("prefferedDelivery") as string) ?? "",
+//     preferredContactMethod:
+//       (formData.get("preferredContactMethod") as string) ?? "",
+//     issueDescription: (formData.get("issueDescription") as string) ?? "",
+//   };
+//   return {
+//     success: true,
+//     message: "",
+//     error: "",
+//     formInput,
+//   };
+// };
